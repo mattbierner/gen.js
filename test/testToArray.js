d548bb907c390550376cc1a6adfbde37fe17d450
Matt Bierner <mattbierner@gmail.com>
2012-09-14 13:34:49 -0400

    Added toArray test for custom yield and break.

--- test/testToArray.js
+++ test/testToArray.js
@@ -13,11 +13,29 @@ define(['../lib/gen', 'shared'], function(gen, shared){
                 assert.deepEqual(g2(), [0, 1, 2, 3]);
                 assert.throws(g2);
             }],
-            ["Empty ToArray",
+            ["Empty Generator",
             function(){
                 var g = gen(shared.count(0)).toArray();
                 assert.deepEqual(g(), []);
                 assert.throws(g);
+            }],
+            ["Custom Yield",
+            function(){
+                var a;
+                var g = gen.toArray(shared.count(4)).bind(undefined, function(v){ a = v; return function(){} });
+                
+                g();
+                assert.deepEqual(a, [0, 1, 2, 3]);
+                assert.throws(g);
+            }],
+            ["Custom Break",
+            function(){
+                var a;
+                var g = gen.toArray(shared.count(4)).bind(undefined, undefined, function(v){ a = 10; return function(){} });
+                
+                assert.deepEqual(g(), [0, 1, 2, 3]);
+                g();
+                assert.equal(a, 10);
             }],
         ],
     };
