8d8875587c65881f008c6e1c9d689e2522e44e8f
Matt Bierner <mattbierner@gmail.com>
2012-09-14 14:10:21 -0400

    Added custom yield and breaks tests for reduce.

--- test/testReduce.js
+++ test/testReduce.js
@@ -23,13 +23,30 @@ define(['../lib/gen', 'shared'], function(gen, shared){
                 assert.equal(g(), 100);
                 assert.throws(g);
             }],
+            ["Custom Yield",
+            function(){
+                var b;
+                var g = gen(shared.count(4)).reduce(add, 0).bind(undefined, function(v){ b = v; return function(){}});
+                
+                g();
+                assert.equal(b, 6);
+                assert.throws(g);
+            }],
+            ["Custom Break",
+            function(){
+                var b;
+                var g = gen(shared.count(4)).reduce(add, 0).bind(undefined, undefined, function(){ b = 10; return function(){}});
+                
+                assert.equal(g(), 6);
+                g();
+                assert.equal(b, 10);
+            }],
             ["Reduce Stack Size",
             function(){
                 var g = gen.reduce(shared.count(10000), add, 0);
                 assert.equal(g(), 49995000);
                 assert.throws(g);
             }],
-           
         ],
     };
 });
